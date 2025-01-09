4ae15c5f0cbf654c1ad8e9a65f2a99f6a162f804
Siddhi Dabholkar <dabholkarsiddhi10@gmail.com>
2025-01-08 20:20:05 -0600

    First React Route Created

---
+++ client/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "blue-300": "#f0f5ff",
+        "primary": "#faa6ff",
+        "mauve-500": "#faa6ff"
+        
+      },
+      boxShadow: {
+        'primary-500': '0px 4px 6px rgba(250, 166, 255, 0.3)', // Adding shadow-primary using the primary color
+      },
+    },
+  },
+  plugins: [],
+}
+
