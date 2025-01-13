66c38d533f3e557b210872fc23e9ec85ca1a7618
Siddhi Dabholkar <dabholkarsiddhi10@gmail.com>
2025-01-12 22:01:40 -0600

    HashSync - password genSalt

--- api/index.js
+++ api/index.js
@@ -3,28 +3,17 @@ require('dotenv').config(); // Load environment variables
 const express = require('express');
 const app = express();
 var cors = require('cors');
-const { MongoClient } = require('mongodb');
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+
+const bcryptSalt = bcrypt.genSaltSync(10);
+const User = require('./models/User');
 
 // Get MongoDB URI from .env
 const uri = process.env.MONGO_URL;
-const client = new MongoClient(uri);
+//const client = new MongoClient(uri);
 
-async function connectToDatabase() {
-    try {
-        await client.connect();
-        console.log("Connected to MongoDB Atlas!");
-
-        // Select the database (it will be created once we insert data)
-        const database = client.db("InnSightDB"); 
-        console.log("Database selected:", database.databaseName);
-    } catch (error) {
-        console.error("Error connecting to MongoDB Atlas:", error);
-    } finally {
-        await client.close();
-    }
-}
-
-connectToDatabase(); // Start the connection
 
 app.use(express.json());
 app.use(cors({
@@ -32,10 +21,32 @@ app.use(cors({
     origin: 'http://localhost:5173',
 }));
 
+
+// Connect to MongoDB and log success
+mongoose.connect(uri)
+    .then(() => {
+        console.log("Connected to MongoDB database successfully!");
+    })
+    .catch((err) => {
+        console.error("Error connecting to MongoDB:", err);
+    });
+
+
 app.get('/test', (req, res) => {
     res.json('test ok');
 });
 
-app.listen(4000, () => {
+
+app.post('/register', async (req,res)=>{
+    const {name,email,password} = req.body;
+    const userDoc  = await  User.create({
+        name,
+        email,
+        password: bcrypt.hashSync(password, bcryptSalt),
+    });
+    res.json(userDoc);
+})
+
+app.listen(4000, () => { 
     console.log("Server is running on port 4000");
-});
+}); 
